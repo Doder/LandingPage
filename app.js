1149db1dc877a75294013c6de159a976ac723db5
Aleksa <aleksadoderovic@gmail.com>
2017-02-22 16:20:51 +0100

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+//var Test = require('./test.js');
+
+class Test extends React.Component{
+    render(){
+        return(
+            <div>
+             <h1>Hello React</h1>
+             
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <Test/>,
+    document.getElementById("main")
+);
---
+++ test.js
@@ -0,0 +1,13 @@
+
+
+class Test extends React.Component{
+    render(){
+        return(
+            <div>
+             <h1>Hello React</h1>
+            </div>
+        );
+    }
+}
+
+module.exports = Test;
