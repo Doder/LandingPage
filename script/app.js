c4ca66f2cc6bcd7ff33157aad49096bdc8e9b48d
Aleksa <aleksadoderovic@gmail.com>
2017-02-22 18:04:16 +0100

    add top navigation bar

--- script/app.js
+++ script/app.js
@@ -1,14 +1,31 @@
-class Test extends React.Component{
+class TopNav extends React.Component{
     render(){
         return(
             <div>
+                <ul className="topnav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+
+class Page extends React.Component{
+    render(){
+        return(
+            <div>
+            <TopNav/>
+            <div>
              <h1>Hello React!</h1>
-             
+            </div>
             </div>
         );
     }
 }
 ReactDOM.render(
-    <Test/>,
+    <Page/>,
     document.getElementById("main")
 );
