19e581a5f371e4df32fb44101a7568bb81baf021
Aleksa <aleksadoderovic@gmail.com>
2017-02-22 16:46:13 +0100

    clean up project

--- script/app.js
+++ script/app.js
@@ -1,10 +1,8 @@
-//var Test = require('./test.js');
-
 class Test extends React.Component{
     render(){
         return(
             <div>
-             <h1>Hello React</h1>
+             <h1>Hello React!</h1>
              
             </div>
         );
