7669c51d24859cf1655c58642113176eaccf676d
Aleksa <aleksadoderovic@gmail.com>
2017-02-23 21:01:51 +0100

    add navbar, background and some text

--- script/app.js
+++ script/app.js
@@ -1,12 +1,27 @@
 class TopNav extends React.Component{
     render(){
         return(
-            <div>
+            <div className="have-margins" id="topnav">
                 <ul className="topnav">
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
+            </div>
+        );
+    }
+}
+
+class SingnUp extends React.Component{
+    render(){
+        return(
+            <div id="signup">
+                <div id="text" className="have-margins">
+                    <h1>Hello There!</h1>
+                    <p>This is landing page I created using React.</p>
+                    <p>Sign up for more free content.</p>
+                    
+                 </div>
             </div>
         );
     }
@@ -18,9 +33,7 @@ class Page extends React.Component{
         return(
             <div>
             <TopNav/>
-            <div>
-             <h1>Hello React!</h1>
-            </div>
+            <SingnUp/>
             </div>
         );
     }
